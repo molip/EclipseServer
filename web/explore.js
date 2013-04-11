c514a5bdfb83ddb1a36d0c3fbb58cf3ee050d885
Jon <jxxwhite@gmail.com>
2013-04-11 20:55:56 +0100

    Stage objects for explore action

--- web/explore.js
+++ web/explore.js
@@ -1,6 +1,57 @@
 var Explore = {}
 
 ///////////////////////////////////////////////////////////////////////////////
+// Stages
+
+Explore.ChoosePosStage = function(positions)
+{
+	this.positions = positions
+	this.selected = {}
+	this.pos_idx = -1
+}
+
+Explore.ChoosePosStage.prototype.OnHexMouseDown = function(x, y)
+{
+	for (var i = 0; i < this.positions.length; ++i)
+		if (this.positions[i].x == x && this.positions[i].y == y)
+		{
+			this.selected.x = x
+			this.selected.y = y
+			this.pos_idx = i
+			document.getElementById('choose_explore_pos_btn').disabled = false
+			return true
+		}
+	return false
+}
+
+Explore.ChoosePosStage.prototype.OnDraw = function(ctx)
+{
+	for (var i = 0; i < this.positions.length; ++i)
+		Map.DrawCentred(ctx, Map.img_explore, this.positions[i].x, this.positions[i].y)
+}
+
+///////////////////////
+
+Explore.ChooseHexStage = function(x, y)
+{
+	this.selected = { x:x, y:y }
+	this.hexes = [] // id, rotations, rot_idx
+	this.hex_idx = 0
+}
+
+Explore.ChooseHexStage.prototype.AddHex = function(id, rotations)
+{
+	var hex = { id:id, rot_idx:0, rotations:rotations }
+	this.hexes.push(hex)
+}
+
+Explore.ChooseHexStage.prototype.OnDraw = function(ctx)
+{
+	var hex = this.hexes[this.hex_idx]
+	Map.DrawHex(ctx, new Map.Hex(hex.id, this.selected.x, this.selected.y, hex.rotations[hex.rot_idx]))
+}
+
+///////////////////////////////////////////////////////////////////////////////
 // Input
 
 Explore.OnCommandChoosePos = function(elem)
@@ -8,21 +59,17 @@ Explore.OnCommandChoosePos = function(elem)
 	ShowActionElement('choose_explore_pos')
 	Map.selecting = true
 
-	data.action = {}
-	data.action.positions = []
-	data.action.OnHexMouseDown = Explore.OnHexMouseDown
-	data.action.OnDraw = Explore.OnDrawPositions
-	data.action.selected = {}
-
-	var positions = GetChildElements(elem, 'pos')
-	for (var i = 0; i < positions.length; ++i)
+	var positions = []
+	var pos_elems = GetChildElements(elem, 'pos')
+	for (var i = 0; i < pos_elems.length; ++i)
 	{
-		var pos = {}
-		pos.x = positions[i].getAttribute('x')
-		pos.y = positions[i].getAttribute('y')
-		data.action.positions.push(pos)
+		var x = pos_elems[i].getAttribute('x')
+		var y = pos_elems[i].getAttribute('y')
+		var pos = { x:x, y:y }
+		positions.push(pos)
 	}
-	data.action.pos_idx = -1
+
+	data.action = new Explore.ChoosePosStage(positions)
 	
 	var can_skip = IsTrue(elem.getAttribute('can_skip'))
 	
@@ -37,29 +84,23 @@ Explore.OnCommandChooseHex = function(elem)
 	Map.selecting = false
 	ShowActionElement('choose_explore_hex')
 
-	data.action = {}
-	data.action.selected = {}
-	data.action.selected.x = Number(elem.getAttribute('x'))
-	data.action.selected.y = Number(elem.getAttribute('y'))
-	data.action.hexes = [] // id, rotations, rot_idx
-	data.action.hex_idx = 0
-	data.action.OnDraw = Explore.OnDrawHex
+	var x = Number(elem.getAttribute('x'))
+	var y = Number(elem.getAttribute('y'))
+
+	data.action = new Explore.ChooseHexStage(x, y)
 	
 	var hexes = GetChildElements(elem, 'hex')
 	for (var i = 0; i < hexes.length; ++i)
 	{
-		var hex = {}
-		hex.id = hexes[i].getAttribute('id')
-		hex.rot_idx = 0
-		hex.rotations = []
+		var id = hexes[i].getAttribute('id')
+		var rotations = []
 		
-		var rotations = GetChildElements(hexes[i], 'rotation')
-		for (var j = 0; j < rotations.length; ++j)
-			hex.rotations.push(rotations[j].getAttribute('steps'))
+		var rot_elems = GetChildElements(hexes[i], 'rotation')
+		for (var j = 0; j < rot_elems.length; ++j)
+			rotations.push(rot_elems[j].getAttribute('steps'))
 		
-		data.action.hexes.push(hex)
+		data.action.AddHex(id, rotations)
 	}
-	//alert(hexes.length)
 
 	ShowElementById('choose_explore_hex_switch_btn', hexes.length > 1, true)
 	
@@ -127,37 +168,4 @@ Explore.Switch = function()
 {
 	data.action.hex_idx = (data.action.hex_idx + 1) % data.action.hexes.length
 	Map.DrawActionLayer()
-}
-
-Explore.OnHexMouseDown = function(x, y)
-{
-	if (data.action && data.action.positions)
-	{
-		for (var i = 0; i < data.action.positions.length; ++i)
-			if (data.action.positions[i].x == Map.hot.x && data.action.positions[i].y == Map.hot.y)
-			{
-				data.action.selected.x = Map.hot.x
-				data.action.selected.y = Map.hot.y
-				data.action.pos_idx = i
-				document.getElementById('choose_explore_pos_btn').disabled = false
-				return true
-			}
-	}
-	return false
-}
-
-Explore.OnDrawPositions = function(ctx)
-{
-	Assert(data.action.positions, "Explore.OnDrawPositions")
-
-	for (var i = 0; i < data.action.positions.length; ++i)
-		Map.DrawCentred(ctx, Map.img_explore, data.action.positions[i].x, data.action.positions[i].y)
-}
-
-Explore.OnDrawHex = function(ctx)
-{
-	Assert(data.action.selected && data.action.hexes, "Explore.OnDrawHex")
-
-	var hex = data.action.hexes[data.action.hex_idx]
-	Map.DrawHex(ctx, new Map.Hex(hex.id, data.action.selected.x, data.action.selected.y, hex.rotations[hex.rot_idx]))
 }
