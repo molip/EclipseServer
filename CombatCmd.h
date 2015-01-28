4e49bc6c7c76869c051c41ade03d3cc15bc8e36e
Jon White <jxxwhite@gmail.com>
2015-01-28 20:17:18 +0000

    Can't undo combat commands. Allows command stack purge.

--- CombatCmd.h
+++ CombatCmd.h
@@ -11,6 +11,7 @@ public:
 
 	virtual void UpdateClient(const Controller& controller, const LiveGame& game) const override;
 	virtual ProcessResult Process(const Input::CmdMessage& msg, CommitSession& session) override;
+	virtual bool CanUndo() const override { return false; }
 };
 
 class CombatDiceCmd : public Cmd
@@ -21,6 +22,7 @@ public:
 
 	virtual void UpdateClient(const Controller& controller, const LiveGame& game) const override;
 	virtual ProcessResult Process(const Input::CmdMessage& msg, CommitSession& session) override;
+	virtual bool CanUndo() const override { return false; }
 
 	virtual void Save(Serial::SaveNode& node) const override;
 	virtual void Load(const Serial::LoadNode& node) override;
